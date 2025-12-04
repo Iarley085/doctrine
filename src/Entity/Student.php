87ee80f06eebc4af96c2a7649cedf4daad4943cd
Iarley Vasconcelos <iarlleyvasconcellos@gmail.com>
2025-12-04 14:42:38 -0300

    iarley

--- src/Entity/Student.php
+++ src/Entity/Student.php
@@ -1,23 +1,33 @@
 <?php
-
 namespace Alura\Doctrine\Entity;
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping\Column;
 use Doctrine\ORM\Mapping\Entity;
 use Doctrine\ORM\Mapping\Id;
 use Doctrine\ORM\Mapping\GeneratedValue;
+use Doctrine\ORM\Mapping\OneToMany;
+use mysql_xdevapi\Collection;
 
 #[Entity]
 class Student
 {
-    #[Id]
-    #[GeneratedValue]
-    #[Column]
-    public readonly int $id;
+    #[Id, Column, GeneratedValue]
+    public  int $id;
+    #[OneToMany (targetEntity: Phones::class, mappedBy: "student")]
+    public  Collection $phones;
     public function __construct(
-
         #[Column]
         public readonly string $name
     ){
+        $this->phones = new ArrayCollection();
+    }
+    public function addPhone(Phones $phone)
+    {
+        $this->phones->add($phone);
+        $phone->setStudent($this);
+    }
+    public function phones(): iterable
+    {
+        return $this->phones();
     }
 }
-$student = new Student(name: 'dede');
